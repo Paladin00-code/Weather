cbac13f42388533d8432e58357357106efa49840
Covert <paladin00@mail.ru>
2018-08-21 15:35:45 +0300

    Не работает(((

--- js/script.js
+++ js/script.js
@@ -14,14 +14,18 @@ new Vue({
 			var self=this;
 			var urlweather = 'https://samples.openweathermap.org/data/2.5/weather?q='+self.city+'&appid=b6907d289e10d714a6e88b30761fae22';
 
-			this.$https.get(urlweather).then(function(response){
-	            var obj = response.content.toJSON();
-	            this.summary = obj.currently.summary;
-	            this.humidity = 'humidity: '+obj.currently.humidity.toString()+'%';
-	            this.windSpeed = 'wind: '+obj.currently.windSpeed.toString()+' mph';
-	            this.temperature = Math.round(obj.currently.temperature).toString() + '°';
-	            this.image(obj.currently.icon.toString());
-        	})
+			$.get(urlweather,
+				function(response) {
+					var obj = response.content.toJSON();
+					this.summary = obj.currently.summary;
+					this.humidity = 'humidity: '+obj.currently.humidity.toString()+'%';
+					this.windSpeed = 'wind: '+obj.currently.windSpeed.toString()+' mph';
+					this.temperature = Math.round(obj.currently.temperature).toString() + '°';
+				}, "json"
+			);	
+			
+			
+			
 		}
 	}
 });
